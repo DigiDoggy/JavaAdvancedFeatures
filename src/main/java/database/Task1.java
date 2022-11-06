07893b3668f36dc043a637bf267c8fd24dd2949e
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2022-11-06 12:25:37 +0200

    added Test comment

--- src/main/java/database/Task1.java
+++ src/main/java/database/Task1.java
@@ -56,6 +56,8 @@ public class Task1 {
                 "year Integer NOT NULL," +
                 " PRIMARY KEY (id))";
 
+        //git test
+
        Statement statement = con.createStatement();
        statement.executeUpdate(SQL);
 
